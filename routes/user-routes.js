2d84b14755bb4137703e87fc51232f4793e9cf32
BudiartoSalim <budiarto.salim91@gmail.com>
2020-09-04 00:31:10 +0700

    should be done but untested

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,9 +2,8 @@ const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/user-controller.js')
 
-
 router.post(`/register`, UserController.postRegister)
-
 router.post(`/login`, UserController.postLogin)
+router.post('/tokensignin', UserController.googleLoginHandler)
 
 module.exports = router
