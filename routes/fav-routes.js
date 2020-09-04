7e0764c2e763c9eba5a9a11f4904e270996a7ac6
claudiovivaldi12 <claudiovivaldi389@gmail.com>
2020-09-04 12:00:02 +0700

    on_test

--- routes/fav-routes.js
+++ routes/fav-routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const authentication = require('../middlewares/authentication.js')
+
+router.use(authentication)
 
 
 module.exports = router
--- routes/resto-routes.js
+++ routes/resto-routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const authentication = require('../middlewares/authentication.js')
 const RestoranController = require('../controllers/resto-controller.js')
+
+router.use(authentication)
 
 module.exports = router
